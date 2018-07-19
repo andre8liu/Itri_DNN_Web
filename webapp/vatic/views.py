4a9a44e476884c704c1098b76c07cd87c5e85233
Andre Liu <and.liu345@gmail.com>
2018-07-19 18:27:03 +0800

    Successfully transfered json and converted, just need images

--- webapp/vatic/views.py
+++ webapp/vatic/views.py
@@ -1,22 +1,217 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
-from django.views.decorators.csrf import csrf_exempt
+from . import read
+from PIL import Image
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+from django.conf import settings
+
+
+# coding=utf-8
+import json
+import cv2
+import os
+from importlib import reload
+import sys
+reload(sys)
 # Create your views here.
 
+# DONE need to find blob and send that in the post request under data and
+# DONEimages under images
+# save POST.get("data") into a file called beforeYoloData.json in a scripto
+# save POST.get("images") into folder named images
+# call convertToYolo to change beforeYoloData.json to yolo data
+# install and configure darknet for server
 
 
 class viaView(View):
-    #def index(request):
-     #return render(request, 'via.html')
+    # def index(request):
+     # return render(request, 'via.html')
 
-
-    def get(self,request):
+    def get(self, request):
         print("hey!!")
         testView = viaView()
-        #return HttpResponse("<h1>Hello from python</h1>")
-        return render(request, 'via.html', {'testView': testView} )
+        # return HttpResponse("<h1>Hello from python</h1>")
+        return render(request, 'via.html', {'testView': testView})
 
-    def post(self,request):
-        print(request.POST.get("data"))
+    def post(self, request):
+
+        # print(request.FILES)
+
+        # SAVES JSON
+        jsondata = json.loads(request.POST.get("data[]"))
+        print(jsondata)
+        with open('data.json', 'w') as outfile:
+            json.dump(jsondata, outfile)
+
+        convertToYolo()
+
+        # SAVES IMAGES!!!!!!
+        # do we need to preserve the name of the image file?
+        # data = request.FILES['file']
+        # imagePathName = 'images/' + str(data)
+        # path = default_storage.save(imagePathName, ContentFile(data.read()))
+       # tmp_file = os.path.join(settings.MEDIA_ROOT, path)
+
+        # print(str(data))
+
         return HttpResponse("hey from post return")
+        # need
+        # so html file is going to have many post requests,
+        # starts with for loop in html file to post every picture in the array so that
+
+
+def test():
+    print("testing outside class function")
+
+
+def check_contain_chinese(check_str):
+    #for ch in check_str.decode('utf-8'):
+     #   if u'\u4e00' <= ch <= u'\u9fff':
+     #       return True
+    return False
+
+
+def convertToYolo():
+    imgdirname = './media/images/'
+    jsonname = 'data.json'
+    namefile = 'labels.names'
+
+    # creating files and getting filenames
+    lbldirname = imgdirname.rstrip('images/')+'/labels/'
+    os.system('mkdir -p '+lbldirname)
+    # listname = jsonname.strip('.json')
+    listname = 'imagePaths'
+    listdata = open(listname, 'wb')
+    labelNames = open(namefile, 'w+')  # creates names file
+
+    # for names file
+    objDict = {}
+    objcount = 0
+    count = 0
+    
+    with open(jsonname, 'r') as f:
+        data = json.loads(f.readline())
+        # print data
+        print(len(data))
+        for key1 in data.keys():  # goes through each file
+
+            filename = imgdirname+data[key1]['filename']  # gets file name
+            print(filename, count, type(filename))
+            listdata.write(filename.encode('gbk'))
+            listdata.write('\n'.encode('gbk'))
+
+            if os.path.isfile(filename):
+                print("FOUNDIT")
+                if check_contain_chinese(filename):
+                    print(filename + ' is chinese')
+                else:
+                    # reading in picture
+                    image = cv2.imread(
+                        filename, cv2.IMREAD_IGNORE_ORIENTATION | cv2.IMREAD_COLOR)
+                    # getting second set of keys
+                    rectangles = data[key1]['regions']
+                    # returns number of rows columns and channels
+                    height_image, width_image, _ = image.shape
+                    print(len(rectangles.keys()), image.shape,
+                          height_image, width_image)
+                    with open(lbldirname+'/'+os.path.splitext(os.path.basename(filename))[0]+'.txt', 'w') as ff:
+                        for key2 in rectangles.keys():
+                            objType = rectangles[key2]["region_attributes"]
+                            obj = str(objType["Animal"])
+                            if not obj in objDict:
+                                objDict[obj] = objcount
+                                labelNames.write(obj)
+                                labelNames.write(' ')
+                                objcount += 1
+                            xywh = rectangles[key2]["shape_attributes"]
+                            x = int(xywh["x"])
+                            y = int(xywh["y"])
+                            w = int(xywh["width"])
+                            h = int(xywh["height"])
+                            xn = float(xywh["x"])/width_image
+                            yn = float(xywh["y"])/height_image
+                            wn = float(xywh["width"])/width_image
+                            hn = float(xywh["height"])/height_image
+                            ff.write('%d %1.5f %1.5f %1.5f %1.5f\n' %
+                                     (objDict[obj], xn+wn/2, yn+hn/2, wn, hn))
+                            print('%d %1.5f %1.5f %1.5f %1.5f' %
+                                  (objDict[obj], xn+wn/2, yn+hn/2, wn, hn))
+                            image = cv2.rectangle(
+                                image, (x, y), (x+w, y+h), (255, 0, 0), 1)
+                    # cv2.imshow('test', image) DONT NEED TO SHOW IMAGE
+                    # cv2.waitKey(0)
+                    count += 1
+
+
+"""
+
+def convertToYolo():
+    sys.setdefaultencoding('utf-8')
+    imgdirname = './images/'
+    jsonname = str(sys.argv[1])
+    namefile = 'labels.names'
+
+    # creating files and getting filenames
+    lbldirname = imgdirname.rstrip('images/')+'/labels/'
+    os.system('mkdir -p '+lbldirname)
+    listname = jsonname.strip('.json')
+    listdata = open(listname, 'wb')
+    labelNames = open(namefile, 'w+') #creates names file
+
+    objDict = {}
+    objcount = 0
+    count = 0
+    
+    with open(jsonname, 'r') as f:
+        data = json.loads(f.readline())
+        # print data
+        print(len(data))
+        for key1 in data.keys():  # goes through each file
+
+            filename = imgdirname+data[key1]['filename']  # gets file name
+            print(filename, count)
+            listdata.write(filename.encode('gbk')+'\n')
+
+            if os.path.isfile(filename):
+                if check_contain_chinese(filename):
+                    print(filename + ' is chinese')
+                else:
+                    # reading in picture
+                    image = cv2.imread(
+                        filename, cv2.IMREAD_IGNORE_ORIENTATION | cv2.IMREAD_COLOR)
+                    # getting second set of keys
+                    rectangles = data[key1]['regions']
+                    # returns number of rows columns and channels
+                    height_image, width_image, _ = image.shape
+                    print(len(rectangles.keys()), image.shape,
+                          height_image, width_image)
+                    with open(lbldirname+'/'+os.path.splitext(os.path.basename(filename))[0]+'.txt', 'w') as ff:
+                        for key2 in rectangles.keys():
+                            objType = rectangles[key2]["region_attributes"]
+                            obj = str(objType["Animal"])
+                            if not objDict.has_key(obj):
+                                objDict[obj] = objcount
+                                labelNames.write(obj)
+                                labelNames.write(' ')
+                                objcount += 1
+                            xywh = rectangles[key2]["shape_attributes"]
+                            x = int(xywh["x"])
+                            y = int(xywh["y"])
+                            w = int(xywh["width"])
+                            h = int(xywh["height"])
+                            xn = float(xywh["x"])/width_image
+                            yn = float(xywh["y"])/height_image
+                            wn = float(xywh["width"])/width_image
+                            hn = float(xywh["height"])/height_image
+                            ff.write('%d %1.5f %1.5f %1.5f %1.5f\n' %
+                                     (objDict[obj], xn+wn/2, yn+hn/2, wn, hn))
+                            print('%d %1.5f %1.5f %1.5f %1.5f' %
+                                  (objDict[obj], xn+wn/2, yn+hn/2, wn, hn))
+                            image = cv2.rectangle(
+                                image, (x, y), (x+w, y+h), (255, 0, 0), 1)
+                    # cv2.imshow('test', image) DONT NEED TO SHOW IMAGE
+                    # cv2.waitKey(0)
+                    count += 1
+"""
